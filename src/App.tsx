17c9bf5863d637f5875c859d699432b0e9a5c558
Kristian Mandrup <kmandrup@gmail.com>
2020-05-01 16:13:45 +0100

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import { mxGraph, mxRubberband, mxClient, mxUtils, mxEvent } from "mxgraph-js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  const divGraph = useRef(null);
+
+  useEffect(() => {
+    if (!mxClient.isBrowserSupported()) {
+      mxUtils.error("Browser is not supported!", 200, false);
+    } else {
+      mxEvent.disableContextMenu(divGraph.current);
+      const graph = new mxGraph(divGraph.current);
+      new mxRubberband(graph);
+      const parent = graph.getDefaultParent();
+      graph.getModel().beginUpdate();
+
+      try {
+        const v1 = graph.insertVertex(parent, null, "Hello,", 20, 20, 80, 30);
+        const v2 = graph.insertVertex(parent, null, "World!", 200, 150, 80, 30);
+        const e1 = graph.insertEdge(parent, null, "", v1, v2);
+      } finally {
+        graph.getModel().endUpdate();
+      }
+    }
+  });
+
+  return <div className="graph-container" ref={divGraph} id="divGraph" />;
+};
 
 export default App;

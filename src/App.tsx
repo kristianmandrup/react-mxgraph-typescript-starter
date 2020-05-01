20232fd9e085394fe857579be3890e2edf4a1546
Kristian Mandrup <kmandrup@gmail.com>
2020-05-01 17:12:48 +0100

    it works :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useRef } from "react";
-import { mxGraph, mxRubberband, mxClient, mxUtils, mxEvent } from "mxgraph-js";
+import { mxgraphFactory } from "ts-mxgraph";
+const { mxGraph, mxRubberband, mxClient, mxUtils, mxEvent } = mxgraphFactory({
+  mxLoadResources: false,
+  mxLoadStylesheets: false,
+});
 
 const App: React.FC = () => {
   const divGraph = useRef(null);
@@ -8,8 +12,10 @@ const App: React.FC = () => {
     if (!mxClient.isBrowserSupported()) {
       mxUtils.error("Browser is not supported!", 200, false);
     } else {
+      console.log({divGraph})
       mxEvent.disableContextMenu(divGraph.current);
-      const graph = new mxGraph(divGraph.current);
+      const element: Element | undefined = divGraph.current || undefined
+      const graph = new mxGraph(element);
       new mxRubberband(graph);
       const parent = graph.getDefaultParent();
       graph.getModel().beginUpdate();
@@ -17,7 +23,8 @@ const App: React.FC = () => {
       try {
         const v1 = graph.insertVertex(parent, null, "Hello,", 20, 20, 80, 30);
         const v2 = graph.insertVertex(parent, null, "World!", 200, 150, 80, 30);
-        const e1 = graph.insertEdge(parent, null, "", v1, v2);
+        // const e1 = 
+        graph.insertEdge(parent, null, "", v1, v2);
       } finally {
         graph.getModel().endUpdate();
       }

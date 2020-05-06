8460952cf2ac715c794d35a61567072bd86a7987
Kristian Mandrup <kmandrup@gmail.com>
2020-05-06 21:51:41 +0100

    CellTooltip

--- src/toolkit/Cell.ts
+++ src/toolkit/Cell.ts
@@ -5,6 +5,40 @@ const { mxGraph } = mxgraphFactory({
   mxLoadStylesheets: false,
 });
 
+export class CellTooltip {
+  graph: any
+
+  constructor(graph: any) {
+    this.graph = graph
+  }
+
+  init() {
+    this.graph.getTooltipForCell = this.getTooltipForCell
+  }
+  getTooltipForCell(cell) {
+    const { graph } = this
+    const getTooltipForCell = graph.getTooltipForCell;
+    const model = graph.getModel()
+    
+    if (!cell) return ''
+    let tip = '';
+    var src = model.getTerminal(cell, true);      
+    if (src) {
+      tip += this.getTooltipForCell(src) + ' ';
+    }      
+    var parent = model.getParent(cell);      
+    if (model.isVertex(parent)) {
+      tip += this.getTooltipForCell(parent) + '.';
+    }
+    tip += getTooltipForCell.apply(graph, arguments);      
+    const trg = model.getTerminal(cell, false);      
+    if (trg) {
+      tip += ' ' + this.getTooltipForCell(trg);
+    }
+    return tip;
+  } 
+}
+
 export class Cell {
   graph: any
 

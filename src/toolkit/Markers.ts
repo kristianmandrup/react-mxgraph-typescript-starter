fe2b416d688ecf0f9d9ece0d53083d9e1e61f0c7
Kristian Mandrup <kmandrup@gmail.com>
2020-05-06 22:39:17 +0100

    make monitor flexible

--- src/toolkit/Markers.ts
+++ src/toolkit/Markers.ts
@@ -5,19 +5,27 @@ const { mxMarker } = mxgraphFactory({
   mxLoadStylesheets: false,
 });
 
+export const markers = {
+  dash: (canvas, shape, type, pe, unitX, unitY, size, source, sw, filled) => {
+    var nx = unitX * (size + sw + 1);
+    var ny = unitY * (size + sw + 1);
+
+    return () => {
+      canvas.begin();
+      canvas.moveTo(pe.x - nx / 2 - ny / 2, pe.y - ny / 2 + nx / 2);
+      canvas.lineTo(pe.x + ny / 2 - 3 * nx / 2, pe.y - 3 * ny / 2 - nx / 2);
+      canvas.stroke();
+    };
+  }
+}
+
 export class Markers {
+  constructor(public markers: any) {
+
+  }
+
   addMarker(label) {
     // Registers and defines the custom marker
-    mxMarker.addMarker('dash', (canvas, shape, type, pe, unitX, unitY, size, source, sw, filled) => {
-      var nx = unitX * (size + sw + 1);
-      var ny = unitY * (size + sw + 1);
-
-      return () => {
-        canvas.begin();
-        canvas.moveTo(pe.x - nx / 2 - ny / 2, pe.y - ny / 2 + nx / 2);
-        canvas.lineTo(pe.x + ny / 2 - 3 * nx / 2, pe.y - 3 * ny / 2 - nx / 2);
-        canvas.stroke();
-      };
-    });  
+    mxMarker.addMarker('dash', this.markers.dash);  
   }
 }
--- src/toolkit/MonitorWorkflowState.ts
+++ src/toolkit/MonitorWorkflowState.ts
@@ -1,70 +1,105 @@
 import { mxgraphFactory } from "ts-mxgraph";
 
-const { mxUtils, mxConstants } = mxgraphFactory({
+const { mxEvent, mxCellOverlay, mxUtils, mxConstants } = mxgraphFactory({
   mxLoadResources: false,
   mxLoadStylesheets: false,
 });
 
+const states = mws => {
+  const { graph } = mws
+  return {
+    running: (state, cell) => {
+      if (state !== 'Running') return
+      graph.setCellStyles(mxConstants.STYLE_FILLCOLOR, '#f8cecc', [cell]);
+    },
+    waiting: (state, cell) => {
+      if (state !== 'Waiting') return
+      graph.setCellStyles(mxConstants.STYLE_FILLCOLOR, '#fff2cc', [cell]);
+    },
+    completed: (state, cell) => {
+      if (state !== 'Completed') return
+      graph.setCellStyles(mxConstants.STYLE_FILLCOLOR, '#f8cecc', [cell]);
+    },
+    init: (state, cell) => {
+      if (state !== 'Init') return
+      graph.addCellOverlay(cell, mws.createOverlay(graph.warningImage, 'State: '+state));
+    }
+  }
+}
+
 export class MonitorWorkflowState {
   graph: any
+  states: any
 
-  constructor(graph: any) {
+  constructor(graph: any, createStates: any) {
     this.graph = graph
+    this.states = createStates(this)
   }
+
+  get tooltipPostFix() {
+    return '\nLast update: ' + new Date()
+  }
+
+  createOverlay(image, tooltip) {
+    const overlay = new mxCellOverlay(image, tooltip);
+
+    // Installs a handler for clicks on the overlay
+    overlay.addListener(mxEvent.CLICK, (sender, evt) => {
+      mxUtils.alert(tooltip + this.tooltipPostFix);
+    });
+    
+    return overlay;
+  };  
 
   /**
    * Updates the display of the given graph using the XML data
    */
   update(xml) {
     const { graph } = this
-    if (xml != null && xml.length > 0) {
-      var doc = mxUtils.parseXml(xml);
-      
-      if (doc != null && doc.documentElement != null) {
-        var model = graph.getModel();
-        var nodes = doc.documentElement.getElementsByTagName('update');
-        
-        if (nodes != null && nodes.length > 0) {
-          model.beginUpdate();
-          try {
-            for (var i = 0; i < nodes.length; i++) {
-              // Processes the activity nodes inside the process node
-              var id = nodes[i].getAttribute('id');
-              var state = nodes[i].getAttribute('state');
-              
-              // Gets the cell for the given activity name from the model
-              var cell = model.getCell(id);
-              
-              // Updates the cell color and adds some tooltip information
-              if (cell != null) {
-                // Resets the fillcolor and the overlay
-                graph.setCellStyles(mxConstants.STYLE_FILLCOLOR, 'white', [cell]);
-                graph.removeCellOverlays(cell);
+    if (xml === null || xml.length > 0) return
+    var doc = mxUtils.parseXml(xml);
     
-                // Changes the cell color for the known states
-                if (state === 'Running') {
-                  graph.setCellStyles(mxConstants.STYLE_FILLCOLOR, '#f8cecc', [cell]);
-                }
-                else if (state === 'Waiting') {
-                  graph.setCellStyles(mxConstants.STYLE_FILLCOLOR, '#fff2cc', [cell]);
-                }
-                else if (state === 'Completed') {
-                  graph.setCellStyles(mxConstants.STYLE_FILLCOLOR, '#d4e1f5', [cell]);
-                }
-                
-                // Adds tooltip information using an overlay icon
-                if (state !== 'Init') {
-                  // Sets the overlay for the cell in the graph
-                  // graph.addCellOverlay(cell, createOverlay(graph.warningImage, 'State: '+state));
-                }
-              }
-            } // for
-          }
-          finally {
-            model.endUpdate();
-          }
+    if (doc != null && doc.documentElement != null) {
+      var model = graph.getModel();
+      var nodes = doc.documentElement.getElementsByTagName('update');
+      
+      if (nodes != null && nodes.length > 0) {
+        model.beginUpdate();
+        try {
+          this.processNodes(nodes)
+        }
+        finally {
+          model.endUpdate();
         }
       }
     }
+  }
+
+  processNodes(nodes) {
+    nodes.map(this.processNode)
+  }
+
+  processNode = (node) => {  
+    const { graph } = this
+    const model = graph.getModel();
+
+    const id = node.getAttribute('id');
+    const state = node.getAttribute('state');
+    
+    // Gets the cell for the given activity name from the model
+    var cell = model.getCell(id);  
+    // Updates the cell color and adds some tooltip information
+    if (cell === null) return
+    // Resets the fillcolor and the overlay
+    graph.setCellStyles(mxConstants.STYLE_FILLCOLOR, 'white', [cell]);
+    graph.removeCellOverlays(cell);
+  }
+
+  processStates(state, cell) {
+    const { states } = this
+    Object.keys(states).map(key => {
+      const stateFn = states[key]
+      stateFn(state, cell)
+    })
   }
 }
